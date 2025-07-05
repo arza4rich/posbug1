8abcd4e0a0907b05183afa68e28c75716e49387e
arza4rich <arza4rich@gmail.com>
2025-07-05 22:34:01 +0900

    Fix Logo Display in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,7 +68,7 @@ const Header = ({ shouldAnimateCart = false }: HeaderProps) => {
           <div onClick={() => handleNavClick('/')} className="flex items-center space-x-2 cursor-pointer">
             <div className="w-10 h-10 rounded-lg overflow-hidden">
               <img 
-                src="/lovable-uploads/022a8dd4-6c9e-4b02-82a8-703a2cbfb51a.png" 
+                src="https://firebasestorage.googleapis.com/v0/b/injapan-food.appspot.com/o/logo%2Finjapan-food-logo.png?alt=media&token=022a8dd4-6c9e-4b02-82a8-703a2cbfb51a" 
                 alt="Injapan Food Logo" 
                 className="w-full h-full object-cover"
               />
